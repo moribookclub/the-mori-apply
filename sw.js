e23684bdd2b58b1b581680589309a154e9154961
moribookclub <nohjunhyouk@gmail.com>
2026-05-26 19:48:48 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+// 기존 sw.js 맨 위에 추가
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAB9FDVMi7AqnKZeTwbq7QYnHY8JZ2mtOk",
+  authDomain: "the-mori-apply.firebaseapp.com",
+  projectId: "the-mori-apply",
+  storageBucket: "the-mori-apply.firebasestorage.app",
+  messagingSenderId: "306904700616",
+  appId: "1:306904700616:web:2217470905df10786837fd"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body: body,
+    icon: '/icon-192.png'
+  });
+});
 const CACHE = 'mori-apply-v5';
 const ASSETS = [
   '/the-mori-apply/',
