1811b59106f183a19daa31312a2311ea96e9534e
moribookclub <nohjunhyouk@gmail.com>
2026-06-17 17:18:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-const CACHE = 'mori-apply-v57';
+const CACHE = 'mori-apply-v58';
 const ASSETS = [
   '/the-mori-apply/',
   '/the-mori-apply/index.html',
